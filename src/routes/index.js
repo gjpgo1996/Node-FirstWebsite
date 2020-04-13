2f101a423ba05509472a5d8b861f11f4d8b4bf6a
gjpgo1996 <gjpgot@gmail.com>
2020-04-13 04:42:42 +0000

    creacion del front con js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', { title: 'First Website' });
+router.get('/', (req, res) => {
+    res.render('index.html', { title: 'First Website' });
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact.html', { title: 'Contact Page' });
 });
 
 module.exports = router;
