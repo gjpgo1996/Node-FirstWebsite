cc62713950bda374a3e18d9a6cea1ec3d451b941
gjpgo1996 <gjpgot@gmail.com>
2020-04-13 04:02:55 +0000

    creando conexiones al servidor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('server works!');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+
+// settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+
+// routes
+app.use(require('./src/routes/index.js'));
+// static file
+
+// listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
